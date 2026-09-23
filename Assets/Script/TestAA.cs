fc9ee4657ec2c97753dc2f97e41e723c888ed8bb
Steliss <welub12@naver.com>
2026-09-23 18:17:19 +0900

    feat TestScript
    ScriptTest

---
+++ Assets/Script/TestAA.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAA : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
